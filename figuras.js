b73ea96fb59fffe03cdaf1d48ee5864790576096
Carlos <jcsc_0302@hotmail.com>
2022-04-05 23:43:07 -0500

    control por html y llamado por boton

--- figuras.js
+++ figuras.js
@@ -26,7 +26,7 @@ console.log("los lados del cuadrado miden: "
     +ladoTriangulo2+"cm,"
     +baseTriangulo+"cm"
 );*/
-const alturaTriangulo=5.5
+//const alturaTriangulo=5.5
 //console.log("La altura del triangulo es de: "+alturaTriangulo+"cm")
 
 function perimetroTriangulo(lado1,lado2,base){
@@ -38,6 +38,16 @@ function areaTriangulo(base,altura){
     return (base*altura)/2
 }
 //console.log("El are del triangulo es: "+areaTriangulo+"cm^2");
+function alturaTrianguloIsosceles(lado,base){
+    return Math.sqrt((lado**2)-(base**2/4));
+}
+function alturaTrianguloEquilatero(lado){
+    return Math.sqrt(lado*3/2);
+}
+function alturaTrianguloEscaleno(lado1,lado2,base){
+    var s=(lado1+lado2+base)/2;
+    return Math.sqrt(s*(s-lado1)*(s-lado2)*(s-base))*2/base;
+}
 console.groupEnd();
 
 //codigo circulo
@@ -61,3 +71,45 @@ function areaCirculo(radio){
     return (radio*radio)*PI;
 }
 //console.log("El area del circulo es : "+areaCirculo+"cm")
+
+//Interaccion con HTML
+//Cuadrado
+function calcularPerimetroCuadrado(){
+    const input=document.getElementById("InputCuadrado");
+    const value=input.value;
+
+    const perimetro=perimetroCuadrado(value);
+    alert(perimetro)
+}
+function calcularAreaCuadrado(){
+    const input=document.getElementById("InputCuadrado");
+    const value=input.value;
+
+
+    const perimetro=areaCuadrado(value);
+    alert(perimetro)
+}
+//Triangulo
+function calcularPerimetroTriangulo(){
+    const lado1=document.getElementById("InputTrianguloLado1").value|0;
+    const lado2=document.getElementById("InputTrianguloLado2").value|0;
+    const base=document.getElementById("InputTrianguloLadoBase").value|0;
+
+    const perimetro=perimetroTriangulo(lado1,lado2,base);
+    alert(perimetro)
+}
+function calcularAreaTriangulo(){
+    const lado1=document.getElementById("InputTrianguloLado1").value|0;
+    const lado2=document.getElementById("InputTrianguloLado2").value|0;
+    const base=document.getElementById("InputTrianguloLadoBase").value|0;
+    var altura;
+    if(lado1===lado2===base){
+         altura=alturaTrianguloEquilatero(lado1);
+    }else if(lado1===lado2){
+         altura=alturaTrianguloIsosceles(lado1,base);
+    }else{
+         altura=alturaTrianguloEscaleno(lado1,lado2,base);
+    }
+    const area=areaTriangulo(base,altura);
+    alert(area)
+}
