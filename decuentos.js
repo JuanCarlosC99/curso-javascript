32e175adae902a95360e8e330b85cfffcebe4a36
Carlos <jcsc_0302@hotmail.com>
2022-04-11 16:48:02 -0500

    se modifico el diseño de descuento y se agrego css

--- decuentos.js
+++ decuentos.js
@@ -10,6 +10,25 @@
 //     precioConDescuento
 // });
 
+const coupons=[
+    {
+        name:"Noesunreloj",
+        discount: 50
+    },
+    {
+        name:"Cupon",
+        discount: 5
+    },
+    {
+        name:"Dame Un Descuento",
+        discount: 10
+    },
+    {
+        name:"12345",
+        discount: 25
+    },
+];
+
 function calcularPrecioConDescuento(precio,descuento){
     const porcentajePrecioConDescuento=100-descuento;
     const precioConDescuento=(precio*porcentajePrecioConDescuento)/100;
@@ -17,7 +36,17 @@ function calcularPrecioConDescuento(precio,descuento){
 }
 function pricediscount(){
     const inputPrice=document.getElementById("InputPrice").value;
-    const inputDiscount=document.getElementById("InputDiscount").value;
-    const priceDiscount=calcularPrecioConDescuento(inputPrice,inputDiscount);
+    const couponValue=document.getElementById("InputCoupon").value;
+   
+    const  isCouponValueValid = function(coupons){
+        return coupons.name===couponValue;
+    }
+    const userCoupon= coupons.find(isCouponValueValid);
+
+    if(!userCoupon){
+        document.getElementById("tdicount").innerText="Tu descuento no es valido";
+    }
+    const priceDiscount=calcularPrecioConDescuento(inputPrice,userCoupon.discount);
+    document.getElementById("tdicount").innerText="Tu descuento es de "+userCoupon.discount+"%";
     document.getElementById("ResultP").innerText="El Precio Con Descuento Son: "+priceDiscount;
-}
+}   
